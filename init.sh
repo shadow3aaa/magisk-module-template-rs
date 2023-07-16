ae02edeab91a5ca56506b92bc0e1a0cd48db7ba4
shadow3aaa <1394025007@qq.com>
2023-07-16 21:17:03 +0800

    modified:   init.sh

--- init.sh
+++ init.sh
@@ -1,13 +1,13 @@
 #/usr/bin/bash
 
-if [ "$1" = "--help" ] || [ "$1" = "-h" ] || [ "$1" = "" ] then
+if [[ "$1" = "--help" || "$1" = "-h" || "$1" = "" ]]; then
     echo "帮助:"
     echo "需要初始化模板仓库名"
     echo "初始化修改只能执行一次, 并且此脚本初始化后自动删除, 谨慎选择"
     echo "示例: bash ./init.sh --name foo"
     exit 0
 else if [ "$1" = "--name" ]; then
-    name = $2
+    name=$2
 fi
 
 if [ "$name" = "" ]; then
